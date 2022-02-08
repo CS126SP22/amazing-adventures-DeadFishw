8dacb326c1af4a17a8a254bd2bee299bcc5d5251
DeadFishw <46125791+DeadFishw@users.noreply.github.com>
2022-02-08 02:19:54 -0600

    finish Direction.java

---
+++ src/main/java/student/adventure/Direction.java
@@ -0,0 +1,28 @@
+package student.adventure;
+
+public class Direction {
+    private String directionName;
+    private String room;
+
+    public Direction() { }
+    public Direction(String setDirection, String setRoom) {
+        directionName = setDirection;
+        room = setRoom;
+    }
+
+    public String getDirectionName() {
+        return directionName;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setDirectionName(String direction) {
+        this.directionName = direction;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+}
