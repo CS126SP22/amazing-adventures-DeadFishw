81151f41a20f53d9fc37835298d35af5cfc8521f
DeadFishw <46125791+DeadFishw@users.noreply.github.com>
2022-02-08 02:22:47 -0600

    implement main for running the game

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import student.adventure.Character;
+import student.adventure.GameEngine;
+import student.adventure.Layout;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Wishing you good luck on your Adventure!
+        GameEngine engine = new GameEngine();
+        File file = new File("src/main/resources/siebel.json");
+        ObjectMapper mapper = new ObjectMapper();
+        Layout layout = mapper.readValue(file, Layout.class);
+
+        // Wishing you good luck on your Adventure!
+        engine.start(layout);
     }
 }
