37f3f52a238ba09e3e97fd8132b5319dbd372bfd
DeadFishw <46125791+DeadFishw@users.noreply.github.com>
2022-02-08 02:20:13 -0600

    finish Item.java and Layout.java

---
+++ src/main/java/student/adventure/Item.java
@@ -0,0 +1,14 @@
+package student.adventure;
+
+public class Item {
+    private String itemName;
+    public Item() {}
+
+    public void setItemName(String name) {
+        this.itemName = name;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+}
---
+++ src/main/java/student/adventure/Layout.java
@@ -0,0 +1,31 @@
+package student.adventure;
+
+public class Layout {
+    String startingRoom;
+    String endingRoom;
+    Room[] rooms;
+    public void setEndingRoom(String endingRoom) {
+        this.endingRoom = endingRoom;
+    }
+
+
+    public void setRooms(Room[] rooms) {
+        this.rooms = rooms;
+    }
+
+    public void setStartingRoom(String startingRoom) {
+        this.startingRoom = startingRoom;
+    }
+
+    public Room[] getRooms() {
+        return rooms;
+    }
+
+    public String getEndingRoom() {
+        return endingRoom;
+    }
+
+    public String getStartingRoom() {
+        return startingRoom;
+    }
+}
